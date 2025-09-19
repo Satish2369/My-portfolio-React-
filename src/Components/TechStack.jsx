986a5090700026935c99d32208569774aa5143e7
satish2369 <satishmourya410@gmail.com>
2025-09-19 18:07:22 +0530

    images added

--- src/Components/TechStack.jsx
+++ src/Components/TechStack.jsx
@@ -1,36 +1,100 @@
 
 
 import React from 'react'
+import { useState } from 'react';
 
 const TechStack = () => {
 
+  
+
+    const [activeTab,setActiveTab]=useState("All");
+
+const techStack = {
+  "All": [
+        "/Images/HTML5.svg",
+    "/Images/CSS3.svg",
+    "/Images/JavaScript.svg",
+    "/Images/React.svg",
+    "/Images/Tailwind CSS.svg",
+        "/Images/Node.js.svg",
+        "/Images/Express.svg",
+        "/Images/MongoDB.svg",
+    "/Images/Mongoose.js.svg",
+    "/Images/Firebase.svg",
+        "/Images/AWS.svg",
+    "/Images/Cloudflare.svg",
+    "/Images/Postman.svg",
+    "/Images/GitHub.svg"
+   
+  ],
+  "Frontend": [
+    "/Images/HTML5.svg",
+    "/Images/CSS3.svg",
+    "/Images/JavaScript.svg",
+    "/Images/React.svg",
+    "/Images/Tailwind CSS.svg"
+  ],
+  "Backend": [
+    "/Images/Node.js.svg",
+    "/Images/Express.svg"
+   
+  ],
+  "Database": [
+    "/Images/MongoDB.svg",
+    "/Images/Mongoose.js.svg",
+    "/Images/Firebase.svg"
+  ],
+  "Tools&platform": [
+        "/Images/AWS.svg",
+    "/Images/Cloudflare.svg",
+    "/Images/Postman.svg",
+    "/Images/GitHub.svg"
+  ],
+  "others": [
+    "/Images/Java.svg"
+  ]
+}
+
+
+   const handleOnClick = (service)=>{
+        
+
+    setActiveTab(service);
+
+    console.log(activeTab);
 
 
 
+   }
 
-    
+
 
 
   return (
-    <div className=' ml-[4vw]     mt-[8vw] font-[Founder_Grotesk] h-screen'>
+    <div className=' ml-[4vw]  mt-[8vw] font-[Founder_Grotesk] h-screen'>
  
-         <div className='text-8xl  '>TECH-STACK & SKILLS</div>
+         <div className='text-8xl  font-semibold'>TECH-STACK & SKILLS</div>
            
 
          <div className=' mt-[1vw] ml-[0.7vw] flex gap-4 text-white text-3xl  border-b-2'>
 
-             <div className='cursor-pointer' >All</div>
-             <div className='cursor-pointer'>Frontend</div>
-             <div className='cursor-pointer'>Backend</div>
-             <div className='cursor-pointer'>Databases</div>
-             <div className='cursor-pointer'>Tools & platform</div>
-             <div className='cursor-pointer'>Others</div>
+         {["All","Frontend","Backend","Database","Tools&platform","others"].map((service,index)=> <div key={index} onClick={()=>handleOnClick(service)}  className='cursor-pointer text-red ' >{service}</div> )
+            
+            
+            }
+
+           
 
 
 
          </div>
 
-         <div className=''>
+         <div className=' flex flex-wrap gap-8 p-6'>
+
+
+
+           {techStack[activeTab].map((link,index) => <img key={index} className='h-[7vw] w-[7vw]' src={link} alt="" /> )}
+       
 
 
 
